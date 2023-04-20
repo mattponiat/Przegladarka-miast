3c5ef07ec1b100269907ad2cb8d01552a3a8e948
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-04-20 23:15:45 +0200

    refactor: :lipstick: Use modals on nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,65 @@
 const Header = () => {
   return (
-    <header className="w-full h-16 flex py-5 px-20 items-center text-slate-900 justify-between border-b-[#E5E7EB] border">
+    <header className="w-full h-16 flex py-5 px-20 items-center text-slate-900 justify-between border-gray-200 border-b">
       <a href="/" className="text-xl font-bold">
         Przeglądarka miast
       </a>
       <div>
         <nav className="flex gap-4 font-medium text-base">
-          <span>O nas</span>
-          <span>Pomoc</span>
-          <span>Kontakt</span>
+          <label htmlFor="my-modal-1" className="cursor-pointer">
+            O nas
+          </label>
+          <input type="checkbox" id="my-modal-1" className="modal-toggle" />
+          <label htmlFor="my-modal-1" className="modal cursor-pointer">
+            <label className="modal-box relative" htmlFor="">
+              <h3 className="text-lg font-bold">O nas</h3>
+              <p className="py-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                obcaecati praesentium blanditiis et. Consequuntur optio tenetur
+                eos! Cupiditate nesciunt omnis est praesentium dolores illum,
+                veniam voluptatum consectetur vel nulla cumque officiis fugit
+                similique iste repellendus iusto pariatur amet voluptatibus
+                quos! Ullam fugiat voluptas id nam doloremque facere odio
+                aliquid reprehenderit?
+              </p>
+            </label>
+          </label>
+          <label htmlFor="my-modal-2" className="cursor-pointer">
+            Pomoc
+          </label>
+          <input type="checkbox" id="my-modal-2" className="modal-toggle" />
+          <label htmlFor="my-modal-2" className="modal cursor-pointer">
+            <label className="modal-box relative" htmlFor="">
+              <h3 className="text-lg font-bold">Pomoc</h3>
+              <p className="py-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                obcaecati praesentium blanditiis et. Consequuntur optio tenetur
+                eos! Cupiditate nesciunt omnis est praesentium dolores illum,
+                veniam voluptatum consectetur vel nulla cumque officiis fugit
+                similique iste repellendus iusto pariatur amet voluptatibus
+                quos! Ullam fugiat voluptas id nam doloremque facere odio
+                aliquid reprehenderit?
+              </p>
+            </label>
+          </label>
+          <label htmlFor="my-modal-3" className="cursor-pointer">
+            Kontakt
+          </label>
+          <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+          <label htmlFor="my-modal-3" className="modal cursor-pointer">
+            <label className="modal-box relative" htmlFor="">
+              <h3 className="text-lg font-bold">Kontakt</h3>
+              <p className="py-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                obcaecati praesentium blanditiis et. Consequuntur optio tenetur
+                eos! Cupiditate nesciunt omnis est praesentium dolores illum,
+                veniam voluptatum consectetur vel nulla cumque officiis fugit
+                similique iste repellendus iusto pariatur amet voluptatibus
+                quos! Ullam fugiat voluptas id nam doloremque facere odio
+                aliquid reprehenderit?
+              </p>
+            </label>
+          </label>
         </nav>
       </div>
     </header>
