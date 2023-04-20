fe3ba61ceb9717e0251eaddcea32e3df420454d8
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-04-20 23:15:19 +0200

    feat: :sparkles: Use react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Header from "./components/Header.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Header />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
