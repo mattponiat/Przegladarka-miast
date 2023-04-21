bf7150181e709e06ddfe13bfbd87e84e73c93a80
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-04-21 02:57:56 +0200

    refactor: :lipstick: Add button for adding new cities
    Cleanup css styles

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 const SideBar = () => {
   return (
-    <div className="w-96 border-gray-200 full-height border-r pl-14 text-slate-900">
-      <div className="flex flex-col px-6">
-        <div className="flex flex-col border-gray-200 border-b pt-8 pb-6">
+    <div className="full-height text-slate-90 w-full max-w-sm border-r border-gray-200 px-2 pl-14 text-base">
+      <div className="full-height flex flex-col px-4 pb-6">
+        <div className="flex flex-col border-b border-gray-200 pb-6 pt-8">
           <label htmlFor="search" className="mb-1 font-medium">
             Wyszukaj
           </label>
@@ -10,7 +12,7 @@ const SideBar = () => {
             id="search"
             name="search"
             type="search"
-            className="w-full border border-gray-200 rounded-md text-sm py-2 pl-3 mb-4"
+            className="mb-4 w-full rounded-md border border-gray-200 py-2 pl-3"
             placeholder="Wpisz nazwę miasta"
           />
           <label htmlFor="voivodeship" className="mb-1 font-medium">
@@ -19,20 +21,31 @@ const SideBar = () => {
           <select
             id="voivodeship"
             name="voivodeship"
-            className="w-full border border-gray-200 rounded-md text-sm py-2 pl-3"
+            className="w-full rounded-md border border-gray-200 py-2 pl-3"
+            defaultValue="Wybierz województwo"
           >
-            <option selected disabled>
-              Wybierz województwo
-            </option>
+            <option disabled>Wybierz województwo</option>
             <option>2</option>
             <option>3</option>
           </select>
         </div>
-        <div className="text-slate-900">
-          <button className="w-full flex items-start border-gray-200 border rounded-md text-sm py-2 pl-3 mt-10">
-            Jakies miasto
-          </button>
+        <div className="flex flex-col gap-2 pt-8 text-slate-900">
+          <Link
+            to={`miasto/warszawa`}
+            className="flex w-full items-start rounded-md border border-gray-200 py-2 pl-3"
+          >
+            Miasto
+          </Link>
+          <Link
+            to={`miasto/poznan`}
+            className="flex w-full items-start rounded-md border border-gray-200 py-2 pl-3"
+          >
+            Miasto
+          </Link>
         </div>
+        <button className="mt-auto w-full rounded-md bg-slate-900 p-2 text-white hover:bg-slate-800">
+          Dodaj nowe miasto
+        </button>
       </div>
     </div>
   );
