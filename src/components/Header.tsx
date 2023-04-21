e562e95235288e9eba6a381934f80ee70e21fefa
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-04-21 02:58:18 +0200

    refactor: :lipstick: Cleanup styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
-    <header className="w-full h-16 flex py-5 px-20 items-center text-slate-900 justify-between border-gray-200 border-b">
+    <header className="flex h-16 w-full items-center justify-between border-b border-gray-200 px-20 py-5 text-slate-900">
       <a href="/" className="text-xl font-bold">
         Przeglądarka miast
       </a>
       <div>
-        <nav className="flex gap-4 font-medium text-base">
+        <nav className="flex gap-4 text-base font-medium">
           <label htmlFor="my-modal-1" className="cursor-pointer">
             O nas
           </label>
