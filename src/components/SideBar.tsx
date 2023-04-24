74a0a059e352644b68f5f9962a6d9a01d4ebd617
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-04-24 16:14:47 +0200

    feat: :sparkles: Implement search and select functionality

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { Cities } from "../types/types";
+import Input from "./ui/Input";
+import Label from "./ui/Label";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "./ui/Select";
 
 type SideBarProps = {
   cities: Cities[];
@@ -8,10 +17,17 @@ type SideBarProps = {
 
 const SideBar = ({ cities }: SideBarProps) => {
   const [search, setSearch] = React.useState("");
+  const [voivodeship, setVoivodeship] = React.useState("");
 
-  //Function to filter cities based on search input
-  const filterCities = React.useMemo(() => {
+  //Filter cities based on search input and selected voivodeship. Returns filtered data.
+  const filteredCities = React.useMemo(() => {
     let result = cities;
+
+    if (voivodeship !== "") {
+      result = cities.filter(
+        (city) => city.voivodeship.toLowerCase() === voivodeship.toLowerCase()
+      );
+    }
 
     if (search) {
       result = cities.filter((city) =>
@@ -20,48 +36,68 @@ const SideBar = ({ cities }: SideBarProps) => {
     }
 
     return result;
-  }, [cities, search]);
+  }, [cities, search, voivodeship]);
+
+  //Filter voivodeship to remove duplicates in select and sort them alphabetically
+  const uniqueCities = cities
+    .filter(
+      (value, index, array) =>
+        array.findIndex((t) => t.voivodeship === value.voivodeship) === index
+    )
+    .sort((a, b) => {
+      if (a.voivodeship < b.voivodeship) {
+        return -1;
+      }
+      if (a.voivodeship > b.voivodeship) {
+        return 1;
+      }
+      return 0;
+    });
 
   return (
-    <div className="full-height text-slate-90 w-full max-w-sm border-r border-gray-200 px-2 pl-14 text-base">
-      <div className="full-height flex flex-col px-4 pb-6">
-        <div className="flex flex-col border-b border-gray-200 pb-6 pt-8">
-          <label htmlFor="search" className="mb-1 font-medium">
+    <div className="full-height sticky top-0 w-full max-w-sm border-r border-gray-200 px-2 pl-14 text-base text-slate-900">
+      <div className="full-height flex flex-col pb-6">
+        <div className="flex flex-col border-b border-gray-200 px-4 pb-6 pt-8">
+          <Label htmlFor="search" className="mb-[6px] font-medium">
             Wyszukaj
-          </label>
-          <input
-            id="search"
-            name="search"
+          </Label>
+          <Input
             type="search"
-            className="mb-4 w-full rounded-md border border-gray-200 py-2 pl-3"
+            id="search"
+            className="mb-4"
             placeholder="Wpisz nazwę miasta"
             value={search}
             onChange={(e) => {
               setSearch(e.currentTarget.value);
             }}
           />
-          <label htmlFor="voivodeship" className="mb-1 font-medium">
+          <Label htmlFor="voivodeship" className="mb-[6px] font-medium">
             Województwo
-          </label>
-          <select
-            id="voivodeship"
-            name="voivodeship"
-            className="w-full rounded-md border border-gray-200 py-2 pl-3"
-            defaultValue=""
-          >
-            <option value="">Wybierz województwo</option>
-            {cities.map((city, i) => (
-              <option
-                key={`${city.voivodeship}, ${i}`}
-                value={city.voivodeship}
-              >
-                {city.voivodeship}
-              </option>
-            ))}
-          </select>
+          </Label>
+          <Select onValueChange={(value) => setVoivodeship(value)}>
+            <SelectTrigger
+              id="voivodeship"
+              className={
+                voivodeship === "" ? "text-slate-400" : "text-slate-900"
+              }
+            >
+              <SelectValue placeholder="Wybierz województwo" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="">Wybierz województwo</SelectItem>
+              {uniqueCities.map((city, i) => (
+                <SelectItem
+                  value={city.voivodeship}
+                  key={`${city.voivodeship}, ${i}`}
+                >
+                  {city.voivodeship}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
-        <div className="flex flex-col gap-2 pt-8 text-slate-900">
-          {filterCities.map((city, i) => (
+        <div className="flex flex-col gap-2 px-4 pt-8 text-sm text-slate-900">
+          {filteredCities.map((city, i) => (
             <Link
               to={`miasto/${city.name.toLowerCase()}`}
               className="flex w-full items-start rounded-md border border-gray-200 py-2 pl-3"
@@ -71,9 +107,11 @@ const SideBar = ({ cities }: SideBarProps) => {
             </Link>
           ))}
         </div>
-        <button className="mt-auto w-full rounded-md bg-slate-900 p-2 text-white hover:bg-slate-800">
-          Dodaj nowe miasto
-        </button>
+        <div className="mx-4 mt-auto text-sm">
+          <button className="w-full rounded-md bg-slate-900 p-2 text-white transition-colors hover:bg-slate-700">
+            Dodaj nowe miasto
+          </button>
+        </div>
       </div>
     </div>
   );
