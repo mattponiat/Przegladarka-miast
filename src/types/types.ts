054962dbf9649fded9402affd78cd53adbc9aab6
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-04-26 03:41:16 +0200

    refactor: :recycle: Refactor types
    Add id on Cities, Add FormFieldProps

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,14 @@
 export type Cities = {
+  id?: number;
   name: string;
   voivodeship: string;
   description: string;
   picture_url: string;
   known_places: string[];
   links: string[];
+};
+
+export type FormFieldProps = {
+  city: Cities;
+  updateCity: React.Dispatch<Partial<Cities>>;
 };
