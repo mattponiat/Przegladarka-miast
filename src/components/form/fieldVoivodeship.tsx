f55f349853f6d38fe2da60bf4ad13318f5219c9b
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-04-26 19:45:32 +0200

    refactor: :recycle: Move allVoivodeships array into new file

--- src/components/form/fieldVoivodeship.tsx
+++ src/components/form/fieldVoivodeship.tsx
@@ -7,24 +7,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
-
-const allVoivodeships = [
-  "Dolnośląskie",
-  "Kujawsko-Pomorskie",
-  "Lubelskie",
-  "Lubuskie",
-  "Łódzkie",
-  "Mazowieckie",
-  "Opolskie",
-  "Podkarpackie",
-  "Podlaskie",
-  "Pomorskie",
-  "Śląskie",
-  "Świętokrzyskie",
-  "Warmińsko-Mazurskie",
-  "Wielkopolskie",
-  "Zachodniopomorskie",
-];
+import { allVoivodeships } from "../../data/allVoivodeships";
 
 const FieldVoivodeship = ({ city, updateCity }: FormFieldProps) => {
   return (
@@ -32,16 +15,30 @@ const FieldVoivodeship = ({ city, updateCity }: FormFieldProps) => {
       <Label htmlFor="voivodeship" className="w-fit font-medium">
         Województwo
       </Label>
-      <Select onValueChange={(value) => updateCity({ voivodeship: value })}>
+      <Select
+        onValueChange={(value) => updateCity({ voivodeship: value })}
+        defaultValue={
+          allVoivodeships.filter(
+            (voivodeship) => voivodeship === city.voivodeship
+          )
+            ? city.voivodeship
+            : ""
+        }
+        name="voivodeship"
+      >
         <SelectTrigger
           className={
-            city?.voivodeship === "" ? "text-slate-400" : "text-slate-900"
+            city.voivodeship === "" ? "text-slate-400" : "text-slate-900"
           }
           id="voivodeship"
+          name="voivodeship"
         >
           <SelectValue placeholder="Wybierz województwo" />
         </SelectTrigger>
         <SelectContent>
+          <SelectItem value="" disabled hidden>
+            Wybierz województwo
+          </SelectItem>
           {allVoivodeships.map((voivodeship) => (
             <SelectItem key={voivodeship} value={voivodeship}>
               {voivodeship}
---
+++ src/data/allVoivodeships.ts
@@ -0,0 +1,18 @@
+export const allVoivodeships = [
+  "Dolnośląskie",
+  "Kujawsko-Pomorskie",
+  "Lubelskie",
+  "Lubuskie",
+  "Łódzkie",
+  "Małopolskie",
+  "Mazowieckie",
+  "Opolskie",
+  "Podkarpackie",
+  "Podlaskie",
+  "Pomorskie",
+  "Śląskie",
+  "Świętokrzyskie",
+  "Warmińsko-Mazurskie",
+  "Wielkopolskie",
+  "Zachodniopomorskie",
+];
